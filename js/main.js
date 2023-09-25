ea63a0f408d63c3b00350f19cb5738831f8426e5
Oleg <53155703+OlegTV@users.noreply.github.com>
2023-09-25 10:39:17 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+$(function(){
+	$('.design-slider').slick({
+		dots: false,
+		slidesToShow: 4,
+		variableWidth: true,
+		prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg">',
+		nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg">',
+		responsive: [
+			{
+				breakpoint: 361,
+				settings: {
+					variableWidth: false,
+					slidesToShow: 1,
+				}
+			}
+		]
+	});
+});
